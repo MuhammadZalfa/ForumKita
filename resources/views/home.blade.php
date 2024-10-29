a18f163a4aed483d68deff5192c221bc78031f7c
MuhammadZalfa <kirigaya.org@gmail.com>
2024-10-29 19:58:45 +0700

    selesai footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,7 +42,7 @@
                     <a class="nav-link text-nowrap" href="">Log in</a>
                   </li>
                   <li class="nav-item ps-1 pe-0">
-                    <a class="btn btn-primary-white" href="">Log in</a>
+                    <a class="btn btn-primary-white" href="">Register</a>
                   </li>
                 </ul>
               </div>
@@ -89,10 +89,10 @@
                 <div class="col-12 col-lg-4 mb-3">
                   <div class="card">
                     <a href="#">
-                      <h3>Cara menambah validasi custom di Laravel 11?</h3>
+                      <h3>'Property of Non-Object' Error</h3>
                     </a>
                     <div>
-                      <p class="mb-5">Saya sedang mengerjakan aplikasi blog di Laravel 8. Ada 4 peran pengguna, di antaranya ...</p>
+                      <p class="mb-5">I'm trying to access a pivot table column value in Blade. When I dd() the following line, I...</p>
                       <div class="row">
                         <div class="col me-1 me-lg-2">
                           <a href=""><span class="badge rounded-pill text-bg-light ">Eloquent</span></a>
@@ -115,10 +115,10 @@
                 <div class="col-12 col-lg-4 mb-3">
                   <div class="card">
                     <a href="#">
-                      <h3>Cara menambah validasi custom di Laravel 11?</h3>
+                      <h3>Middleware go looping sometimes</h3>
                     </a>
                     <div>
-                      <p class="mb-5">Saya sedang mengerjakan aplikasi blog di Laravel 8. Ada 4 peran pengguna, di antaranya ...</p>
+                      <p class="mb-5">Can you guys know why my user entering a redirect loop sometimes? It's happened very ...</p>
                       <div class="row">
                         <div class="col me-1 me-lg-2">
                           <a href=""><span class="badge rounded-pill text-bg-light ">Eloquent</span></a>
@@ -130,7 +130,7 @@
                             </a>
                           </div>
                           <span class="fs-12px">
-                            <a href="" class="me-1 fw-bold">Faza  Radit</a>
+                            <a href="" class="me-1 fw-bold">Sultan Syaeful</a>
                             <span class="color-gray">7 hours ago</span>
                           </span>
                         </div>
@@ -141,10 +141,10 @@
                 <div class="col-12 col-lg-4 mb-3">
                   <div class="card">
                     <a href="#">
-                      <h3>Cara menambah validasi custom di Laravel 11?</h3>
+                      <h3>How to add a custom validation in laravel 11?</h3>
                     </a>
                     <div>
-                      <p class="mb-5">Saya sedang mengerjakan aplikasi blog di Laravel 8. Ada 4 peran pengguna, di antaranya ...</p>
+                      <p class="mb-5">I am working on a blogging application in Laravel 8. There are 4 user roles, among which, the.../p>
                       <div class="row">
                         <div class="col me-1 me-lg-2">
                           <a href=""><span class="badge rounded-pill text-bg-light ">Eloquent</span></a>
@@ -156,7 +156,7 @@
                             </a>
                           </div>
                           <span class="fs-12px">
-                            <a href="" class="me-1 fw-bold">Faza  Radit</a>
+                            <a href="" class="me-1 fw-bold">Agung Rahayu</a>
                             <span class="color-gray">7 hours ago</span>
                           </span>
                         </div>
@@ -167,6 +167,46 @@
               </div>
             </div>
           </section>
+
+          <section class="container min-h-372px d-flex flex-column align-items-center justify-content-center">
+            <h2>Ready to contribute?</h2>
+            <p class="mb-4">Want to make a big impact?</p>
+            <div class="text-center">
+              <a href="" class="btn btn-primary me-2 mb-lg-0"> Sign Up</a>
+              <a href="" class="btn btn-secondary mb-2 mb-lg-0">Join Diskusi</a>
+            </div>
+          </section>
+
+          <footer class="py-80px">
+            <div class="container">
+              <div class="row">
+                <div class="col-12 col-lg-6 mb-5 mb-lg-0">
+                  <img src="{{ url('assets/images/LogoForumKita.png') }}" alt="" class="h-32px mb-4">
+                  <p class="text-white">Empowering the Developer Community <br> to connect, share and learn.</p>
+                </div>
+                <div class="col-12 col-lg-6 me-auto">
+                  <div class="d-flex flex-column flex-lg-row justify-content-end">
+                    <div class="d-flex flex-column me-140px mb-3 mb-lg-0">
+                        <p class="fw-bold fs-5 text-white text-nowrap">
+                          Contact Us
+                        </p>
+                        <ul class="list-unstyled">
+                          <li class="text-white"><a href="mailto:kirigaya.org@gmail.com">kirigaya.org@gmail.com</a></li>
+                        </ul>
+                    </div>
+                    <div class="d-flex flex-column">
+                      <p class="fw-bold fs-5 text-white">Link</p>
+                      <ul class="list-unstyled">
+                        <li class="text-white mb2 "><a href="#">Home</a></li>
+                        <li class="text-white mb2 "><a href="#">Discussion</a></li>
+                        <li class="text-white mb2 "><a href="#">About Us</a></li>
+                      </ul>
+/                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </footer>
 
           <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     </body>
